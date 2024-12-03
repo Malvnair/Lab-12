c53356f61cfe75652857b2c81fd5e6cdc9799261
Malvnair <130719699+Malvnair@users.noreply.github.com>
2024-12-02 20:27:28 -0500

    Modified the plotting code to have "+" markers and  the year column include unique values

--- NairMalavika_Lab12.py
+++ NairMalavika_Lab12.py
@@ -6,13 +6,14 @@ df = pd.read_csv("co2_mm_mlo.csv", skiprows=52, usecols=[0, 3], delimiter=",")
 df.columns = ['year', 'average']  
 
 filtered_data = df[(df['year'] >= 1981) & (df['year'] <= 1990)]
+ 
 
-# Plot
 plt.figure(figsize=(10, 6))
-plt.plot(filtered_data['year'], filtered_data['average'], label='CO2 Levels (1981-1990)')
-plt.xlabel('Year')
+plt.scatter(filtered_data.index, filtered_data['average'], label='CO2 Levels (1981-1990)', marker='+')
+plt.xlabel('Index')
 plt.ylabel('CO2 Levels (ppm)')
 plt.title('CO2 Measurements from 1981 to 1990')
 plt.legend()
 plt.grid()
 plt.show()
+
